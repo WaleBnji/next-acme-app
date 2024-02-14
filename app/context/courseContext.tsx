60bf56d8606b1aa17a3de1903a4be8af086a6525
Wale Bnji <walebnji@gmail.com>
2024-02-14 19:23:37 +0100

    latest commit

--- app/context/courseContext.tsx
+++ app/context/courseContext.tsx
@@ -1,70 +1,70 @@
-import React, { createContext, useState, useContext } from 'react';
-import { nanoid } from 'nanoid';
+// import React, { createContext, useState, useContext } from 'react';
+// import { nanoid } from 'nanoid';
 
-const courses = [
-  {
-    tutorName: 'Oketola Samuel',
-    duration: '3 weeks',
-    course: 'Web Development',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus. Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus.',
-    cost: 'free',
-  },
-  {
-    tutorName: 'Isaac Folarin',
-    duration: '4 weeks',
-    course: 'Mobile Engineering',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus. Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus.',
-    cost: 'paid',
-  },
-  {
-    tutorName: 'Ayele Aduragbemi',
-    duration: '3 weeks',
-    course: 'Digital management',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus. Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus.',
-    cost: 'free',
-  },
-  {
-    tutorName: 'Mohammed Olamilekan',
-    duration: '4 weeks',
-    course: 'Product Design',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus. Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus.',
-    cost: 'paid',
-  },
-];
+// const courses = [
+//   {
+//     tutorName: 'Oketola Samuel',
+//     duration: '3 weeks',
+//     course: 'Web Development',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus. Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus.',
+//     cost: 'free',
+//   },
+//   {
+//     tutorName: 'Isaac Folarin',
+//     duration: '4 weeks',
+//     course: 'Mobile Engineering',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus. Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus.',
+//     cost: 'paid',
+//   },
+//   {
+//     tutorName: 'Ayele Aduragbemi',
+//     duration: '3 weeks',
+//     course: 'Digital management',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus. Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus.',
+//     cost: 'free',
+//   },
+//   {
+//     tutorName: 'Mohammed Olamilekan',
+//     duration: '4 weeks',
+//     course: 'Product Design',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus. Lorem ipsum dolor sit amet, consectetur adipisicing elit.delectus.',
+//     cost: 'paid',
+//   },
+// ];
 
-interface Course {
-  tutorName: string;
-  duration: string;
-  course: string;
-  description: string;
-  cost: string;
-}
+// interface Course {
+//   tutorName: string;
+//   duration: string;
+//   course: string;
+//   description: string;
+//   cost: string;
+// }
 
-interface CourseContextProps {
-  courses: Course[];
-}
-export const CourseContext = createContext<undefined | CourseContextProps>(
-  undefined,
-);
+// interface CourseContextProps {
+//   courses: Course[];
+// }
+// export const CourseContext = createContext<undefined | CourseContextProps>(
+//   undefined,
+// );
 
-export const CourseProvider = (props: { children: React.ReactNode }) => {
-  const [courseList, setCourseList] = useState(courses);
-  const [userInput, setUserInput] = useState('');
-  const value = {
-    courses: courseList,
-  };
+// export const CourseProvider = (props: { children: React.ReactNode }) => {
+//   const [courseList, setCourseList] = useState(courses);
+//   const [userInput, setUserInput] = useState('');
+//   const value = {
+//     courses: courseList,
+//   };
 
-  return (
-    <CourseContext.Provider value={value}>
-      {props.children}
-    </CourseContext.Provider>
-  );
-};
+//   return (
+//     <CourseContext.Provider value={value}>
+//       {props.children}
+//     </CourseContext.Provider>
+//   );
+// };
 
-export const useApp = () => {
-  return useContext(CourseContext);
-};
+// export const useApp = () => {
+//   return useContext(CourseContext);
+// };
